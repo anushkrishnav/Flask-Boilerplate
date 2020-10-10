c028caaa125a6b6399e6a683ed4f7104ba9a05a8
anushkrishnav <anush.venkatakrishna@gmail.com>
2020-10-10 18:08:58 +0530

    removed un required files

--- application/forms.py
+++ application/forms.py
@@ -1,19 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField,BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired
 
 
 class RegisterForm(FlaskForm):
-    username=StringField("Name",validators=[DataRequired()])
-    email=StringField("Email",validators=[DataRequired()])
-    #Github=StringField("Github Url",validators=[DataRequired()])
-    #linkdln=StringField("Linkdln Url",validators=[DataRequired()])
+    username = StringField("Name", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired()])
+    # Github=StringField("Github Url",validators=[DataRequired()])
+    # linkdln=StringField("Linkdln Url",validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    #Tech=StringField("Skills",validators=[DataRequired()])
-    submit=SubmitField("Register Now")
+    # Tech=StringField("Skills",validators=[DataRequired()])
+    submit = SubmitField("Register Now")
+
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Login')
--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,8 @@
 from flask import render_template, redirect, flash, url_for
+from flask_login import current_user, login_user, logout_user
+from .model.models import User
 from application import appp
-
-from .forms import RegisterForm, LoginForm
+from .forms import LoginForm
 
 
 @appp.route('/')
@@ -20,14 +21,25 @@ def blog():
     return render_template('blog.html', blog=True)
 
 
-@appp.route("/login")
+@appp.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.email.data, form.remember_me.data))
-        return redirect('/')
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+
+@appp.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 
 @appp.route("/events")
@@ -35,6 +47,7 @@ def events():
     return render_template('events.html', events=True)
 
 
+'''
 @appp.route("/register",methods=['POST','GET'])
 def register():
     form = RegisterForm()
@@ -70,3 +83,4 @@ def register():
 #     data = GetTable()
 #     data = data[0][2]
 #     return render_template('test.html', data=data)
+'''
